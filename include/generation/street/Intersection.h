ebe34e92ea6329a522cc1c15cd82f063b41be000
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-12-09 20:57:46 -0600

    Added intersection class

--- include/generation/street/Intersection.h
+++ include/generation/street/Intersection.h
@@ -5,9 +5,24 @@
 #ifndef PROCEDURALCITIES_INTERSECTION_H
 #define PROCEDURALCITIES_INTERSECTION_H
 
+#include <vector>
+#include <glm/glm.hpp>
+
+class StreetSegment;
 
 class Intersection {
+    Intersection(glm::vec3 position);
 
+    bool addStartingStreet(StreetSegment*);
+    bool addEndingStreet(StreetSegment*);
+    glm::vec3 getPosition();
+    const std::vector<StreetSegment*>& getStreets();
+
+    ~Intersection();
+
+private:
+    std::vector<StreetSegment*> streets;
+    glm::vec3 position;
 };
 
 
